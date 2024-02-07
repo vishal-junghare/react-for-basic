06f7c279ff980bb0ec2a1f3ebec222bcc7df1cae
vishal-junghare <vishaljunghare0430@gmail.com>
2024-02-07 17:28:02 +0530

    add all file and floder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  var[a,b]= useState(7);
+  return (
+    <div className='w-full h-screen bg-red-900 p-4 '>
+    <h1>{a}</h1>
+    <button  onClick={()=> b(a+1)} className='px-3 py-4 text-green-600 bg-black border-45'>click</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className='text-green-100 p-5 text-center w-32 h-30 bg-black mt-6'>Products</div>
+  )
+}
+
+export default Products
